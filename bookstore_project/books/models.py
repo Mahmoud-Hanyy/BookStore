8d4ea93628253a2a57b8717fced8fa8ceba8d604
Mahmoud <mahmoudhany374@gmail.com>
2025-06-19 22:14:16 +0300

    Added new book attributes in Book Model

--- bookstore_project/books/models.py
+++ bookstore_project/books/models.py
@@ -1,11 +1,28 @@
 from django.db import models
-
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Book(models.Model):
-  title = models.CharField(max_length=200)
-  author = models.CharField(max_length=100)
-  price = models.DecimalField(max_digits=6, decimal_places=2)
+    """
+    Represents a book in the bookstore with extended details.
+    Each book now has a title, description, rate, and views count.
+    """
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True, null=True) 
+    rate = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        default=0.00,
+        validators=[MinValueValidator(0.00), MaxValueValidator(5.00)] # Ensures rate is between 0 and 5
+    )
+    views = models.IntegerField(default=0)
+    def __str__(self):
+        """
+        Returns a string representation of the book,
+        useful for the Django admin site and debugging.
+        """
+        return self.title
 
-  def __str__(self):
-    return self.title
+    class Meta:
+        ordering = ['title']
--- bookstore_project/bookstore_project/settings.py
+++ bookstore_project/bookstore_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bookstore_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookstore_db',
+        'USER': 'bookstore_user',
+        'PASSWORD': 'your_secure_password', # Use the password you set
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
